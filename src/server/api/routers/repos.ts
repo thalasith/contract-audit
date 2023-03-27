a6e12aa6c1cc686e042462e21c2ea5765286bf04
Boris Tsao <boris.tsao@gmail.com>
2023-03-26 20:33:29 -0700

    adding in file page

--- src/server/api/routers/repos.ts
+++ src/server/api/routers/repos.ts
@@ -108,55 +108,61 @@ export const reposRouter = createTRPCRouter({
     return returnData;
   }),
 
-  getFile: protectedProcedure.query(async ({ ctx }) => {
-    const accountId = ctx.session?.user.id;
+  getFile: protectedProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        repoName: z.string(),
+        path: z.string().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const accountId = ctx.session?.user.id;
 
-    if (!accountId) {
-      throw new Error("User not authenticated");
-    }
+      if (!accountId) {
+        throw new Error("User not authenticated");
+      }
 
-    const user = await ctx.prisma.account.findMany({
-      where: { userId: accountId },
-    });
+      const user = await ctx.prisma.account.findMany({
+        where: { userId: accountId },
+      });
 
-    const access_token = user[0]?.access_token;
-    const octokit = new Octokit({
-      auth: access_token, // Replace with your access token or use an environment variable
-    });
+      const access_token = user[0]?.access_token;
+      const octokit = new Octokit({
+        auth: access_token, // Replace with your access token or use an environment variable
+      });
 
-    // const response = await octokit.rest.repos.listForAuthenticatedUser();
+      const response = await octokit.rest.repos.getContent({
+        owner: input.username,
+        repo: input.repoName,
+        path: input.path || "",
+      });
 
-    const response = await octokit.rest.repos.getContent({
-      owner: "thalasith",
-      repo: "decentrahoops",
-      path: "contract/src/lib.rs",
-    });
+      if (Array.isArray(response.data) || response.data.type !== "file") {
+        throw new Error("Path does not point to a file");
+      }
 
-    if (Array.isArray(response.data) || response.data.type !== "file") {
-      throw new Error("Path does not point to a file");
-    }
+      const fileContentBase64 = response.data.content;
+      const fileContent = Buffer.from(fileContentBase64, "base64").toString(
+        "utf8"
+      );
 
-    const fileContentBase64 = response.data.content;
-    const fileContent = Buffer.from(fileContentBase64, "base64").toString(
-      "utf8"
-    );
+      // const openai = new OpenAI.OpenAIApi(configuration);
 
-    // const openai = new OpenAI.OpenAIApi(configuration);
+      // try {
+      //   const completion = await openai.createChatCompletion({
+      //     model: "gpt-3.5-turbo",
+      //     messages: [
+      //       {
+      //         role: "user",
+      //         content: `Tell A bit of context, this is a smart contract within the Near Protocol. Tell me everything that is wrong with the below code from a security perspective and how somebody can exploit each issue.  \n\n${fileContent}'`,
+      //       },
+      //     ],
+      //   });
 
-    // try {
-    //   const completion = await openai.createChatCompletion({
-    //     model: "gpt-3.5-turbo",
-    //     messages: [
-    //       {
-    //         role: "user",
-    //         content: `Tell A bit of context, this is a smart contract within the Near Protocol. Tell me everything that is wrong with the below code from a security perspective and how somebody can exploit each issue.  \n\n${fileContent}'`,
-    //       },
-    //     ],
-    //   });
-
-    // } catch (e) {
-    //   console.log(e);
-    // }
-    return fileContent;
-  }),
+      // } catch (e) {
+      //   console.log(e);
+      // }
+      return fileContent;
+    }),
 });
