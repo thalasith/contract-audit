a13ce3f7856a7fe4d36c28dc1abfc2084c2c6b52
Boris Tsao <boris.tsao@gmail.com>
2023-04-08 17:50:23 -0700

    first draft with keypom

--- src/server/api/routers/keypom.ts
+++ src/server/api/routers/keypom.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import * as keypom from "keypom-js";
 import { env } from "~/env.mjs";
+import path from "path";
+import fs from "fs";
 import {
   createTRPCRouter,
   publicProcedure,
@@ -21,51 +23,58 @@ export const keypomRouter = createTRPCRouter({
     });
 
     const callableContracts = [CONTRACT_ID];
-
     const { dropId, keys } = await keypom.createTrialAccountDrop({
       numKeys: 1,
-      contractBytes: [1000000],
+      contractBytes: [
+        ...fs.readFileSync(
+          path.resolve(__dirname, "../../../../../ext-wasm/trial-accounts.wasm")
+        ),
+      ],
       maxAttachableYoctoPerContract: ["1"],
-      startingBalanceNEAR: 0.5,
+      startingBalanceNEAR: 5,
       callableContracts: callableContracts,
       callableMethods: ["*"],
       maxAttachableNEARPerContract: [1],
-      trialEndFloorYocto: "1000000000000000000000000",
-      trialEndFloorNEAR: 0.33 + 0.3,
+      trialEndFloorYocto: "1",
+      trialEndFloorNEAR: 1,
     });
-
-    // const desiredAccountId = `testthalanos.testnet`;
+    const desiredAccountId = `${dropId}-keypom.testnet`;
     const trialSecretKey = keys?.secretKeys[0] || "";
-    // console.log("desiredAccountId: ", desiredAccountId);
+    console.log(`desiredAccountId: ${JSON.stringify(desiredAccountId)}`);
     console.log(`trialSecretKey: ${JSON.stringify(trialSecretKey)}`);
 
     await keypom.claimTrialAccountDrop({
-      desiredAccountId: "afkljewkljrwthal.testnet",
+      desiredAccountId: desiredAccountId,
       secretKey: trialSecretKey,
     });
 
-    // await keypom.trialCallMethod({
-    //   trialAccountId: desiredAccountId,
-    //   trialAccountSecretKey: trialSecretKey,
-    //   contractId: CONTRACT_ID,
-    //   methodName: "account_id",
-    //   args: { account_id: desiredAccountId },
-    //   attachedDeposit: "0.1",
-    //   attachedGas: "30000000000000",
-    // });
+    return {
+      statusCode: 200,
+      message: `successfully deployed account`,
+    };
+  }),
+  createAudit: publicProcedure.query(async ({ ctx }) => {
+    await keypom.initKeypom({
+      network: "testnet",
+      funder: {
+        accountId: env.FUNDING_ACCOUNT_ID,
+        secretKey: env.FUNDING_ACOUNT_PRIVATE_KEY,
+      },
+    });
 
-    // await keypom.trialCallMethod({
-    //   trialAccountId: desiredAccountId,
-    //   trialAccountSecretKey: trialSecretKey,
-    //   contractId: CONTRACT_ID,
-    //   methodName: "add_audit",
-    //   args: {
-    //     github_name: "flight_shield",
-    //     audit_description: "testing audit description",
-    //   },
-    //   attachedDeposit: "0.1",
-    //   attachedGas: "30000000000000",
-    // });
+    await keypom.trialCallMethod({
+      trialAccountId: "1680994747556-keypom.testnet",
+      trialAccountSecretKey:
+        "2Msx1uyWd8hm1K6tvkCCaPai8yTf8dii4TMAuQrbu3UfVkkKTxyMjZxfgP6GHxdCpPsJTf5zQiX1YCXQgTRexWkw",
+      contractId: CONTRACT_ID,
+      methodName: "add_audit",
+      args: {
+        github_name: "testing keypom",
+        audit_description: "testing keypom description",
+      },
+      attachedDeposit: parseNearAmount("0") + "",
+      attachedGas: "30000000000000",
+    });
     return {
       statusCode: 200,
       message: `successfully deployed account`,
